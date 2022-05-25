3908df84ff22926c74546de0fc4b4eb96b028b7a
Yeong joon <bluehome8626@naver.com>
2022-05-26 08:35:31 +0900

    프로젝트 세팅

---
+++ src/QR.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import QRcode from "qrcode.react";
+
+const QR = () => {
+  return (
+    <Wrapper>
+      <Header>체육대회 관전 바코드</Header>
+      <Box>
+        <QRcode size={280} id="myqr" value="e" />
+      </Box>
+      <Bottom>캡쳐를 꼭 해주세요!!!</Bottom>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  margin-top: 20px;
+  width: 200px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  margin-top: 20px;
+  width: 300px;
+  height: 300px;
+  padding: 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Bottom = styled.div`
+  margin-top: 20px;
+  width: 300px;
+  height: 40px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export default QR;
